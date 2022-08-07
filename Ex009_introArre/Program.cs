a3fb75509930172059a55ae77599c63ab0eaea32
kirill <k.grinikh@gmail.com>
2022-08-07 18:03:41 +0300

    1

---
+++ Ex009_introArre/Program.cs
@@ -0,0 +1,21 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3; 
+    return result;
+}
+
+
+int[] arrey = {15, 24, 37, 845, 54, 67, 72, 84, 95};
+
+int max = Max( 
+    Max(arrey[0], arrey[1], arrey[2]),
+    Max(arrey[3], arrey[4], arrey[5]),
+    Max(arrey[6], arrey[7], arrey[8])
+
+);
+
+Console.WriteLine(max);
+
+
---
+++ Ex010_Metho/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = {15, 25, 37, 48, 54, 65, 73, 87, 93, };
+
+int n = array.Length;
+int find = 48;
+
+int index = 0;
+
+while (index < n);
+
+{
+    if(array [index] == find);
+
+    {
+        Console.WriteLine(index);
+    }
+
+
+    // index = index + 1;
+
+    index++;
+
+}
